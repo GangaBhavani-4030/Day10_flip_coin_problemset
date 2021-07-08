fe6b1a6f70db051708c3de74724df5b7ff14daca
GangaBhavani-4030 <gangabhavani.4030@gmail.com>
2021-07-08 13:33:26 +0530

    flipping coin multiple times and finding who won the game

--- flipcoin_day10.sh
+++ flipcoin_day10.sh
@@ -1,8 +1,27 @@
 echo "Welcome to the hackers on Flipping coin stimulation problem "
-coin=$(( RANDOM % 2 ))
-if(( coin == 0 ))
+echo "enter the number: "
+read n
+hc=0
+tc=0
+for(( i=1; i<=n; i++ ))
+do 
+	coin=$(( RANDOM % 2 ))
+	if(( coin == 0 ))
+	then
+		echo "flip-$i tails"
+		(( tc++ ))
+	else
+		echo "flip-$i heads"
+		(( hc++ ))
+	fi
+done
+echo "heads_count = $hc and tails_count = $tc "
+if(( hc > tc ))
 then
-	echo "tails"
+	echo "heads won the stimulation task"
+elif(( tc > hc ))
+then
+	echo "tails won the stimulation task"
 else
-	echo "heads"
+	echo " it's a tie"
 fi
